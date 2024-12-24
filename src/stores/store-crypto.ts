69b73600d8c7378ca44bd6631e812005609576f5
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-12-23 23:58:59 -0500

    feat( array crypto currencies) se crea el type store para poder usarlo en el store principal, retornando asi la data y asignandosela a cryptocurrencies

--- src/stores/store-crypto.ts
+++ src/stores/store-crypto.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 import { CryptoCurrenciesResponseSchema } from "../schemas/crypto-schema";
+import { CryptoCurrency } from "../types";
+
+type CryptoStore = {
+  cryptocurrencies: CryptoCurrency[];
+  fetchCryptos: () => Promise<void>;
+};
 
 async function getCryptos() {
   // const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";
@@ -10,13 +17,23 @@ async function getCryptos() {
     data: { Data },
   } = await axios.get(url);
   const result = CryptoCurrenciesResponseSchema.safeParse(Data);
-  console.log(result);
+  if (result.success) {
+    // console.log(result.data);
+    return result.data;
+  } else {
+    console.error(result.error);
+    return [];
+  }
 }
 
-export const useCryptoStore = create(() => ({
-  // crypto: [],
-  // setCrypto: (crypto) => set({ crypto }),
-  fetchCryptos: async () => {
-    getCryptos();
-  },
-}));
+export const useCryptoStore = create<CryptoStore>()(
+  devtools((set) => ({
+    cryptocurrencies: [],
+    fetchCryptos: async () => {
+      const cryptocurrencies = await getCryptos();
+      set(() => ({
+        cryptocurrencies,
+      }));
+    },
+  }))
+);
