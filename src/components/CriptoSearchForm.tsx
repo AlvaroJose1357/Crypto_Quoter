c5c3bcb81ceea7a86353a0c8b7e0388c96e0706e
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-12-25 01:23:06 -0500

    feat(event submit)

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { currencies } from "../data";
 import { useCryptoStore } from "../stores/store-crypto";
 import { Pair } from "../types";
+import ErrorMessage from "./ErrorMessage";
 
 export default function CriptoSearchForm() {
   const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies);
   // const { cryptocurrencies } = useCryptoStore();
+  const [error, setError] = useState<string>("");
   const [pair, setPair] = useState<Pair>({
     currency: "",
     cryptocurrency: "",
@@ -17,14 +19,33 @@ export default function CriptoSearchForm() {
       [event.target.name]: event.target.value,
     });
   };
+
+  const hangleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // if (pair.currency.trim() === "" || pair.cryptocurrency.trim() === "") {
+    //   setError("Todos los campos son obligatorios");
+    //   return;
+    // }
+    if (Object.values(pair).includes("")) {
+      setError("Todos los campos son obligatorios");
+      return;
+    }
+    setError("");
+    // si la valicacion pasa consultamos la API
+    console.log(pair);
+  };
   return (
-    <form className="form">
+    <form
+      className="form"
+      onSubmit={hangleSubmit}>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
       <div className="field">
         <label htmlFor="currency">Elige tu Moneda</label>
         <select
           name="currency"
           id="currency"
           className="form-control"
+          value={pair.currency}
           onChange={handleChange}>
           <option value="">- Selecciona tu moneda-</option>
           {currencies.map((currency) => (
@@ -42,6 +63,7 @@ export default function CriptoSearchForm() {
           name="cryptocurrency"
           id="cryptocurrency"
           className="form-control"
+          value={pair.cryptocurrency}
           onChange={handleChange}>
           <option value="">- Selecciona tu criptomoneda-</option>
           {cryptocurrencies.map((crypto) => (
